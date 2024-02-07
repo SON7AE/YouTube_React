47089f47ca07dcaf9b9e52bf63a9c4327fc687b7
SON7AE <bobsot0225@gmail.com>
2024-02-07 14:18:39 +0900

    [개발] YouTube React 강의 - 검색기능 추가

--- src/components/molecules/common/SearchBox.tsx
+++ src/components/molecules/common/SearchBox.tsx
@@ -1,24 +1,27 @@
 import { useState } from "react"
+import { useRecoilState } from "recoil"
+import { searchState } from "@recoil/atoms/searchState"
 import styles from "./SearchBox.module.scss"
 
 function SearchBox() {
+    const [search, setSearch] = useRecoilState(searchState)
     const [text, setText] = useState("")
     const onChange = (event: any) => {
         setText(event.target.value)
     }
     // 엔터키 입력
-    // const handleKeyDown = (e: React.KeyboardEvent) => {
-    //     if (e.key === "Enter") {
-    //         if (text === "") return funcSetValue("korea")
-    //         else return funcSetValue(text) // 작성한 Input 값 부모컴포넌트로 전달
-    //     }
-    // }
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === "Enter") {
+            if (text === "") return setSearch("korea")
+            else return setSearch(text) // 작성한 Input 값 부모컴포넌트로 전달
+        }
+    }
 
     return (
         <div className={styles.searchBox}>
             <div className={styles.searchBox__search}>
-                {/* <input type="text" placeholder="찾으실 이미지를 검색하세요." className={styles.searchBox__search__input} value={text} onChange={onChange} onKeyDown={handleKeyDown} /> */}
-                <input type="text" placeholder="찾으실 이미지를 검색하세요." className={styles.searchBox__search__input} value={text} onChange={onChange} />
+                <input type="text" placeholder="찾으실 이미지를 검색하세요." className={styles.searchBox__search__input} value={text} onChange={onChange} onKeyDown={handleKeyDown} />
+                {/* <input type="text" placeholder="찾으실 이미지를 검색하세요." className={styles.searchBox__search__input} value={text} onChange={onChange} /> */}
                 <img src="/assets/icons/search.svg" alt="" />
             </div>
         </div>
