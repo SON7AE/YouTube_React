205ff42b10ff34c0e9a132dde8c72f872b93bfa5
SON7AE <bobsot0225@gmail.com>
2024-05-08 09:16:12 +0900

    빌드 후 배포용 코드

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -39,7 +39,7 @@ function index() {
             <div className={styles.page__contents}>
                 <div className={styles.page__contents__introBox}>
                     <div className={styles.wrapper}>
-                        <span className={styles.wrapper__title}>PhtoSplash</span>
+                        <span className={styles.wrapper__title}>PhotoSplash</span>
                         <span className={styles.wrapper__desc}>
                             인터넷의 시각 자료 출처입니다. <br />
                             모든 지역에 있는 크리에이터들의 지원을 받습니다.
